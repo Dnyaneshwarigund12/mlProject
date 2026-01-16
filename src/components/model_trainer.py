ef045e255bf9c8989c5b26c790bce6921a04deea
dnyaneshwarigund <dnyaneshwarigund2003@gmail.com>
2026-01-16 15:36:06 +0530

    predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -84,7 +84,7 @@ class ModelTrainer:
                 
             }
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,y_test=y_test,X_test=X_test
-                                            ,models=models)
+                                            ,models=models,param=params)
             
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
