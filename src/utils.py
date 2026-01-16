d86424c0af29539e80658642a1715d61d75c757d
dnyaneshwarigund <dnyaneshwarigund2003@gmail.com>
2026-01-16 15:17:02 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -18,4 +18,24 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            # Predicting the test data
+            y_test_pred=model.predict(X_test)
+
+            # Get the r2 score for the model
+            r2_square=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=r2_square
+
+        return report   
+    except Exception as e:
+        raise CustomException(e,sys)
     
